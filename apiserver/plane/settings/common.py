b8d0f1b7822727728e362337880c2eb61802883c
MostafaMamouni <mostafa-1470@live.fr>
2023-12-19 21:04:11 +0100

    dummy commit

--- apiserver/plane/settings/common.py
+++ apiserver/plane/settings/common.py
@@ -138,6 +138,7 @@ else:
         "default": {
             "ENGINE": "django.db.backends.postgresql",
             "NAME": os.environ.get("POSTGRES_DB"),
+            "PORT": os.environ.get("POSTGRES_PORT"),
             "USER": os.environ.get("POSTGRES_USER"),
             "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
             "HOST": os.environ.get("POSTGRES_HOST"),
--- setup.sh
+++ setup.sh
@@ -8,5 +8,3 @@ export LC_CTYPE=C
 cp ./web/.env.example ./web/.env
 cp ./apiserver/.env.example ./apiserver/.env
 
-# Generate the SECRET_KEY that will be used by django
-echo "SECRET_KEY=\"$(tr -dc 'a-z0-9' < /dev/urandom | head -c50)\""  >> ./apiserver/.env
